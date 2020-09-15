695488d493f83335076f3b85aa4560165ae782cc
mmkobylin <mmkobylin@gmail.com>
2020-09-15 12:15:37 +0100

    Resource for Games created

--- app/Http/Controllers/API/Games.php
+++ app/Http/Controllers/API/Games.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class Drawings extends Controller
+class Games extends Controller
 {
     /**
      * Display a listing of the resource.
