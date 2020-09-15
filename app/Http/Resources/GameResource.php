bc9c562ca9f662246781602ef0c537406689591a
mmkobylin <mmkobylin@gmail.com>
2020-09-15 12:55:42 +0100

    GameResource created

--- app/Http/Resources/GameResource.php
+++ app/Http/Resources/GameResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class Games extends JsonResource
+class GameResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
